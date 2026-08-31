a795c7d91f791428255daf55af41c8b074b157ce
penn <penn@Pennix.local>
2026-08-31 14:43:56 +0800

    chore: rebrand Trellis fork packages

--- packages/cli/src/commands/channel/create.ts
+++ packages/cli/src/commands/channel/create.ts
@@ -4,7 +4,7 @@ import {
   resolveChannelRef,
   type ChannelScope,
   type ChannelType,
-} from "@mindfoldhq/trellis-core/channel";
+} from "@pennixrv/trellis-core/channel";
 
 import {
   parseChannelScope,
